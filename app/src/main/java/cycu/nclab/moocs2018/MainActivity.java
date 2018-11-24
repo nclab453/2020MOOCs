31a4156da9665f4be512aae9e9e603faf1892b61
hhliu <hunghuan.liu@gmail.com>
2018-11-25 05:55:16 +0800

    ConstraintLayout

--- app/src/main/java/cycu/nclab/moocs2018/MainActivity.java
+++ app/src/main/java/cycu/nclab/moocs2018/MainActivity.java
@@ -7,38 +7,38 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity {
 
-    TextView tx;
-    Button bt;
+//    TextView tx;
+//    Button bt;
 
     @Override   // annotation, 註解
     protected void onCreate(Bundle savedInstanceState) { // function, 函式
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.bookkeeping_constraintlayout);
 
-        tx = findViewById(R.id.textView);
-        bt = findViewById(R.id.button);
-
-        bt.setOnClickListener(this);
-        tx.setOnClickListener(this);
-
-    }
-
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.textView:
-                tx.setTextColor(Color.RED);
-                tx.setText("Hello World!");
-                break;
-            case R.id.button:
-                tx.setTextColor(Color.GREEN);
-                tx.setText("點我、點我！");
-                break;
-        }
+//        tx = findViewById(R.id.textView);
+//        bt = findViewById(R.id.button);
+//
+//        bt.setOnClickListener(this);
+//        tx.setOnClickListener(this);
 
     }
+
+//    @Override
+//    public void onClick(View v) {
+//        switch (v.getId()) {
+//            case R.id.textView:
+//                tx.setTextColor(Color.RED);
+//                tx.setText("Hello World!");
+//                break;
+//            case R.id.button:
+//                tx.setTextColor(Color.GREEN);
+//                tx.setText("點我、點我！");
+//                break;
+//        }
+//
+//    }
 
     //    第二種方式，建立獨立的監聽器
 //    private View.OnClickListener myClickListener = new View.OnClickListener() {
