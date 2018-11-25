b6d75b54c08d97b648b2595ccb40c8729963d0b6
hhliu <hunghuan.liu@gmail.com>
2018-11-25 19:41:34 +0800

    activity to activity by handler

---
+++ app/src/main/java/cycu/nclab/moocs2018/Bookkeeping.java
@@ -0,0 +1,110 @@
+package cycu.nclab.moocs2018;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class Bookkeeping extends AppCompatActivity implements View.OnClickListener {
+    final String TAG = this.getClass().getSimpleName();
+    static int count = 0;
+
+    TextView theDate, theTime;
+
+    Button btSave;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bookkeeping_constraintlayout);
+
+        uiInit();
+        count++;
+        Log.d(TAG, "enter onCreate(), #" + count);
+    }
+
+    private void uiInit() {
+        theDate = findViewById(R.id.textView6);
+        theTime = findViewById(R.id.textView7);
+
+        btSave = findViewById(R.id.button);
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        varInit();
+        registerListener();
+        Log.d(TAG, "enter onStart(), #" + count);
+    }
+
+
+    private void varInit() {
+    }
+
+
+    private void registerListener() {
+        theDate.setOnClickListener(this);
+        theTime.setOnClickListener(this);
+        btSave.setOnClickListener(this);
+    }
+
+
+    @Override
+    protected void onStop() {
+        Log.d(TAG, "enter onStop(), #" + count);
+        releaseListener();
+
+        super.onStop();
+    }
+
+    private void releaseListener() {
+        theDate.setOnClickListener(null);
+        theTime.setOnClickListener(null);
+        btSave.setOnClickListener(null);
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        Log.d(TAG, "enter onDestroy(), #" + count);
+        count--; // -- 遞減運算子； 等於 count = count - 1;
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onPause() {
+        Log.d(TAG, "enter onPause(), #" + count);
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "enter onResume(), #" + count);
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "enter onRestart(), #" + count);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button:
+                // 儲存帳務資料
+
+
+                finish();
+                break;
+        }
+
+    }
+}
--- app/src/main/java/cycu/nclab/moocs2018/MainActivity.java
+++ app/src/main/java/cycu/nclab/moocs2018/MainActivity.java
@@ -1,57 +1,120 @@
 package cycu.nclab.moocs2018;
 
-import android.graphics.Color;
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
-//    TextView tx;
-//    Button bt;
+    final String TAG = this.getClass().getSimpleName();
+    static int count = 0;
+    final int demoCase = 1; // 0: click to switch； 1: use handler
+
 
     @Override   // annotation, 註解
     protected void onCreate(Bundle savedInstanceState) { // function, 函式
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_bookkeeping_relative_layout);
+        setContentView(R.layout.activity_main);
 
-//        tx = findViewById(R.id.textView);
-//        bt = findViewById(R.id.button);
-//
-//        bt.setOnClickListener(this);
-//        tx.setOnClickListener(this);
+        count++; // ++: 遞增運算子；等於 count = count + 1;
 
+        Log.d(TAG, "enter onCreate(), #" + count); // 3 + 4 , + : 運算子；3, 4 運算元
     }
 
-//    @Override
-//    public void onClick(View v) {
-//        switch (v.getId()) {
-//            case R.id.textView:
-//                tx.setTextColor(Color.RED);
-//                tx.setText("Hello World!");
-//                break;
-//            case R.id.button:
-//                tx.setTextColor(Color.GREEN);
-//                tx.setText("點我、點我！");
-//                break;
-//        }
-//
-//    }
-
-    //    第二種方式，建立獨立的監聽器
-//    private View.OnClickListener myClickListener = new View.OnClickListener() {
-//        @Override
-//        public void onClick(View v) {
-//            tx.setTextColor(Color.BLUE);
-//        }
-//    };
 
 
-    //    第一種方式，使用xml onClick屬性
-//    public void onButtonClick(View view) {
-//        tx.setTextColor(Color.BLUE);
-//    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "enter onStart(), #" + count);
+
+        switch (demoCase) {
+            case 0:
+                // 找到此Activity的root View，並增加click監聽
+                (this.findViewById(android.R.id.content)).setOnClickListener(this);
+                break;
+            case 1:
+                // 使用Handler自動切換螢幕畫面
+                Message msg = myHandler.obtainMessage();    // 從Global Message Pool裡面取一個message出來，
+                                                            // 比新建立一個有效率。
+                                                            // Ctr+Q 查看指令文件
+                myHandler.sendMessageDelayed(msg, 500); // 0.5秒後執行
+                break;
+        }
+    }
+
+
+
+    @Override
+    protected void onStop() {
+        Log.d(TAG, "enter onStop(), #" + count);
+
+        super.onStop();
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        Log.d(TAG, "enter onDestroy(), #" + count);
+        count--;
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onPause() {
+        Log.d(TAG, "enter onPause(), #" + count);
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "enter onResume(), #" + count);
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "enter onRestart(), #" + count);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case android.R.id.content:
+                startActivity(new Intent(this, Bookkeeping.class));
+
+                // 增加過場動畫
+                overridePendingTransition(android.R.anim.slide_in_left,
+                        android.R.anim.slide_out_right);
+
+                // 強制activity終止
+                MainActivity.this.finish();
+                break;
+        }
+    }
+
+    // ---------------------使用handler的範例------------------------------------------------------
+
+    private Handler myHandler = new Handler(Looper.getMainLooper()) {
+
+        @Override
+        public void handleMessage(Message msg) {
+            startActivity(new Intent(MainActivity.this, Bookkeeping.class));
+
+            // 增加過場動畫
+            overridePendingTransition(android.R.anim.slide_in_left,
+                    android.R.anim.slide_out_right);
+            MainActivity.this.finish();
+        }
+    };
+
+    // -------------------------------------------------------------------------------------------
 
 }
+
